b1001276446f52708c0a07b6143eb6955e735513
Sergey-Russiyan <sergey.russiyan@gmail.com>
2013-08-06 22:00:06 +0300

    commit for further pull request

--- utils.rb
+++ utils.rb
@@ -1,16 +1,29 @@
 module Utils
-  def first_method(arr)
-    arr.reject! {|el| el % 2 == 1}.max
-  end
+  # not-self methods
+  # some useless changes
+  def first_method(arr); arr.reject! {|el| el % 2 == 1}.max; end
 
   def second_method(arr)
-    arr.values.reject! {|el| el % 2 == 1}.max
+    arr.values.reject! {|el| el % 2 == 1}.max 
   end
 
   def third_method(arr)
     arr.keys.reject! {|el| el % 2 == 1}.max
   end
 
+  def first_method_ser_r(array)
+    array.reject! { |element| element % 2 == 1}.max
+  end
+ 
+  def second_method_ser_r(hash={})
+    hash.values.reject! { |element| element % 2 == 1}.max
+  end
+
+  def third_method_ser_r(hash={})
+    hash.keys.reject! { |element| element % 2 == 1}.max
+  end
+
+ # self methods below
   def self.max_even_array_value(arr=[])
     arr.select {|i| i.even?}.max
   end
@@ -57,21 +70,6 @@ module Utils
 
   def self.min_odd_hash_key(hsh={})
     min_odd_array_value hsh.keys
-  end
-
-
-  def first_method_ser_r(array)
-    array.reject! { |element| element % 2 == 1}.max
-  end
-
- 
-  def second_method_ser_r(hash={})
-    hash.values.reject! { |element| element % 2 == 1}.max
-  end
-
-
-  def third_method_ser_r(hash={})
-    hash.keys.reject! { |element| element % 2 == 1}.max
   end
 
 end
