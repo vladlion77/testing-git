30f6915175cac51021031e6eb4353c04e8ad5d11
vladlion <vladlion77@gmail.com>
2013-08-06 22:01:55 +0300

    add method

--- utils.rb
+++ utils.rb
@@ -1,13 +1,13 @@
 module Utils
-  def first_method
-    %w{a b c d e f g}.each {|el| puts el}
+  def self.first_method array
+    array.select {|n| n.even?}.max
   end
 
-  def second_method
-    {a: 1, b: 2, c: 3, d: 4, e: 5}.each {|el| puts el}
+  def self.second_method(hash)
+    hash.values.select {|n| n.even?}.max
   end
 
-  def third_method
-    puts 'abcdefg'
+  def self.third_method
+    hash.keys.select {|n| n.even?}.max
   end
 end
