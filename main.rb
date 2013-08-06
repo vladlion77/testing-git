3fefa30d25ffa07017a46934963555b302d65390
Konstantin Lynda <kostyan1488@gmail.com>
2013-08-06 21:21:40 +0300

    implemented additional utils methods

--- main.rb
+++ main.rb
@@ -1,10 +1,18 @@
 require File.expand_path('utils', File.dirname(__FILE__))
 
-p Utils.max_even_array_value([1, 2, 8, 6, 5])
-p Utils.max_even_array_value([1, 1, 7, 3, 5])
+puts Utils.max_even_array_value([1, 2, 8, 6, 5])
+puts Utils.min_even_array_value([1, 2, 8, 6, 5])
+puts Utils.max_odd_array_value([1, 2, 8, 6, 5])
+puts Utils.min_odd_array_value([1, 2, 8, 6, 5])
 
-p Utils.max_even_hash_value({a: 2, b:6, 7 => 8})
-p Utils.max_even_hash_value({a: 3, b:63, 7 => 1})
 
-p Utils.max_even_hash_key({1 => 3, 6 => 63, 7 => '1'})
-p Utils.max_even_hash_key({1 => 3, 167 => 63, 7 => '1'})
+puts Utils.max_even_hash_value({a: 2, b: 6, 7 => 3, 17 => 5})
+puts Utils.min_even_hash_value({a: 2, b: 6, 7 => 3, 17 => 5})
+puts Utils.max_odd_hash_value({a: 2, b: 6, 7 => 3, 17 => 5})
+puts Utils.min_odd_hash_value({a: 2, b: 6, 7 => 3, 17 => 5})
+
+puts Utils.max_even_hash_key({1 => 3, 6 => 63, 7 => '1', 2 => 's'})
+puts Utils.min_even_hash_key({1 => 3, 6 => 63, 7 => '1', 2 => 's'})
+puts Utils.max_odd_hash_key({1 => 3, 6 => 63, 7 => '1', 2 => 's'})
+puts Utils.min_odd_hash_key({1 => 3, 6 => 63, 7 => '1', 2 => 's'})
+
--- utils.rb
+++ utils.rb
@@ -3,11 +3,47 @@ module Utils
     arr.select {|i| i.even?}.max
   end
 
+  def self.min_even_array_value(arr=[])
+    arr.select {|i| i.even?}.min
+  end
+
+  def self.max_odd_array_value(arr=[])
+    arr.select {|i| i.odd?}.max
+  end
+
+  def self.min_odd_array_value(arr=[])
+    arr.select {|i| i.odd?}.min
+  end
+
   def self.max_even_hash_value(hsh={})
     max_even_array_value hsh.values
   end
 
+  def self.min_even_hash_value(hsh={})
+    min_even_array_value hsh.values
+  end
+
+  def self.max_odd_hash_value(hsh={})
+    max_odd_array_value hsh.values
+  end
+
+  def self.min_odd_hash_value(hsh={})
+    min_odd_array_value hsh.values
+  end
+
   def self.max_even_hash_key(hsh={})
     max_even_array_value hsh.keys
+  end
+
+  def self.min_even_hash_key(hsh={})
+    min_even_array_value hsh.keys
+  end
+
+  def self.max_odd_hash_key(hsh={})
+    max_odd_array_value hsh.keys
+  end
+
+  def self.min_odd_hash_key(hsh={})
+    min_odd_array_value hsh.keys
   end
 end
