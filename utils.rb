49b0073702b8bd4c6c613022a32541b9e3890b71
Sergey-Russiyan <sergey.russiyan@gmail.com>
2013-08-06 22:31:13 +0300

    deleted ALL useless comments

--- utils.rb
+++ utils.rb
@@ -1,7 +1,8 @@
 module Utils
-  # not-self methods
-  # some useless changes
-  def first_method(arr); arr.reject! {|el| el % 2 == 1}.max; end
+  
+  def first_method(arr)
+    arr.reject! {|el| el % 2 == 1}.max
+  end
 
   def second_method(arr)
     arr.values.reject! {|el| el % 2 == 1}.max 
